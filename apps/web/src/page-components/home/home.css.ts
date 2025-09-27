b02ddda1141dea021e1ba23331eeb573e77802e0
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-09-27 16:32:55 +0530

    added home page basic content

--- apps/web/src/page-components/home/home.css.ts
+++ apps/web/src/page-components/home/home.css.ts
@@ -5,3 +5,35 @@ export const grid = style({
 	flexWrap: 'wrap',
 	gap: '1rem',
 });
+
+export const hero = style({
+	display: 'grid',
+	gridColumn: '1/-1' /* full width */,
+	/* height: 400px; */
+	gridTemplateColumns: '1fr 1fr minmax(auto, 700px) 1fr',
+	columnGap: '16px',
+	flexWrap: 'wrap',
+	position: 'relative',
+	alignItems: 'center',
+	justifyContent: 'center',
+	/* text-align: center; */
+	minHeight: 'calc(50vh)',
+	/* max-height: calc(100vh - 100px); */
+});
+
+export const gradientText = style({
+	display: 'inline-block',
+	color: 'transparent',
+	// background: '-webkit-linear-gradient(var(--gradient2))',
+	background: 'linear-gradient(var(--gradient2))',
+	backgroundSize: '400% 100%',
+	backgroundClip: 'text',
+	WebkitBackgroundClip: 'text',
+	WebkitTextFillColor: 'transparent',
+	vars: {
+		'--gradient1':
+			'74deg , #4285f4 0 , #9b72cb 9% , #d96570 20% , #d96570 24% , #9b72cb 35% , #4285f4 44% , #9b72cb 50% , #d96570 56% , #131314 75% , #131314 100%',
+		'--gradient2':
+			'to right top , #d16ba5 , #c777b9 , #ba83ca , #aa8fd8 , #9a9ae1 , #8aa7ec , #79b3f4 , #69bff8 , #52cffe , #41dfff , #46eefa , #5ffbf1',
+	},
+});
--- apps/web/src/page-components/home/home.tsx
+++ apps/web/src/page-components/home/home.tsx
@@ -1,11 +1,41 @@
-'use client';
-
-import { grid } from './home.css';
+// import AnimatedLogo from '@/components/SVG/Logo/AnimatedLogo';
+// import PostListPage from './blog/page';
+import { metadata } from '@/app/layout';
+import { gradientText, hero } from './home.css';
 
 export default function Home() {
 	return (
-		<div className={grid}>
-			<p>Home Page</p>
-		</div>
+		<>
+			<div className={hero}>
+				{/* <AnimatedLogo
+					animateOnce={true}
+					initialInView={false}
+					className={'m-auto animatedLogo'}
+				/> */}
+				<div>
+					<div>
+						<big aria-hidden="true">👋</big>
+						<p>Hi, I am</p> {/* changes for narrator - accessibility */}
+					</div>
+					<h1 className={gradientText}>Anwesh Gangula</h1>
+					<p>{metadata.description}</p>
+					<p>
+						Welcome to my "Digital Garden". This is where I would like to share
+						& showcase:
+					</p>
+					<ul>
+						<li>🏗️ things i'm building</li>
+						<li>🎋 what I'm currently learning</li>
+						<li>🥰 and things that inspire me</li>
+					</ul>
+					{/* <p>
+						Added D3 JS implementation in Nextjs - <Link href={"./d3_Next13"}>here</Link>
+					</p> */}
+				</div>
+			</div>
+			{/* <section className="postList">
+				<PostListPage />
+			</section> */}
+		</>
 	);
 }
