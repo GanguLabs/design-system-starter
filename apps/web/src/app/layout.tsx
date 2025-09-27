406fbeb6ef12cc796d11d2e326cd87c9e6fac448
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-09-27 16:32:41 +0530

    added metadata for head element

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,7 +4,29 @@ import '@ds-starter/ui/css/styles.css';
 import '@repo/styles/globals.css';
 import { container } from '@repo/styles/header.css';
 import '@repo/styles/theme.css';
+import { Metadata } from 'next';
 import Image from 'next/image';
+
+export const metadata: Metadata = {
+	// this add the html `<head/>` element
+	// ref: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+	metadataBase: new URL('https://anweshgangula.github.io/'),
+	title: {
+		default: 'Anwesh Gangula',
+		template: '%s | Anwesh Gangula',
+	},
+	description: '3D, WebGL & Meta enthusiast',
+	// applicationName: "Anwesh Gangula",
+	authors: [
+		{
+			name: 'Anwesh Gangula',
+			url: 'https://anweshgangula.com/',
+		},
+	],
+	generator: 'nextjs, react, blog',
+	// colorScheme: constants.defaultTheme,
+	// themeColor: constants.themeColorMetaDark,
+};
 
 export default function RootLayout({
 	children,
