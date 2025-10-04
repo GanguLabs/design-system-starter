15b2596dc6780d2944e719dce370a3adcf8af2fa
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-10-04 21:28:00 +0530

    added about page

---
+++ apps/web/src/app/about/page.tsx
@@ -0,0 +1,19 @@
+export default function AboutPage() {
+	return (
+		<div>
+			<h1>About Me</h1>
+			<div>
+				<img
+					src="/avatar.jpg"
+					alt="Anwesh Gangula"
+					style={{ width: '150px', borderRadius: '75px' }}
+				/>
+				<h2>Anwesh Gangula</h2>
+			</div>
+			<p>
+				I'm a software developer with a passion for building web applications
+				and sharing knowledge through blogging.
+			</p>
+		</div>
+	);
+}
