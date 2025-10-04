aeb0ff0710778d888bf67f29a1a6195455d8a522
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-10-04 23:13:09 +0530

    added footer

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import HtmlMain from '@/components/html-main/html-main';
+import { SiteFooter } from '@/components/site-footer/site-footer';
 import ThemeSwitcher from '@/components/theme-switcher/theme-switcher';
 import { ThemeProvider } from '@/providers/theme-provider';
 import '@ds-starter/tokens/css/tokens.css';
@@ -91,6 +92,7 @@ export default function RootLayout({
 						<ThemeSwitcher />
 					</div>
 					<HtmlMain>{children}</HtmlMain>
+					<SiteFooter />
 				</ThemeProvider>
 			</body>
 		</html>
