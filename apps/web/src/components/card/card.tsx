58a6a4a701eeef87a1c9f7401843cdc159438881
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-10-05 12:41:22 +0530

    added badge and card component

---
+++ apps/web/src/components/card/card.tsx
@@ -0,0 +1,23 @@
+export default function Card({
+	className,
+	children,
+}: {
+	className?: string;
+	children: React.ReactNode;
+}) {
+	return (
+		<div
+			className={className + ' my_card'}
+			style={{
+				borderRadius: '0.5rem',
+				backgroundColor: '#c9c9c9',
+				padding: '1.5rem',
+				boxShadow:
+					'0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24)',
+				transition: 'box-shadow 0.3s ease-in-out',
+			}}
+		>
+			{children}
+		</div>
+	);
+}
