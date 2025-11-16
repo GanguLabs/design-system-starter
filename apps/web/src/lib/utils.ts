d23452d920563191319b1c072a50286021c09952
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-11-16 15:34:28 +0530

    setup blog page

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,3 +1,4 @@
+import { Post } from '@site/content';
 
 export function formatDate(dateString: string): string {
 	const date = new Date(dateString);
@@ -8,3 +9,8 @@ export function formatDate(dateString: string): string {
 	});
 }
 
+export function sortPostsByDate(posts: Array<Post>): Array<Post> {
+	return posts.sort(
+		(a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+	);
+}
