1beaca9257ce1e4f0412faabe563d3a4a271a893
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-12-06 21:30:27 +0530

    use webpack instead of turbo-repo

--- apps/next-16/next.config.ts
+++ apps/next-16/next.config.ts
@@ -1,8 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+	/* config options here */
+	reactCompiler: true,
+	reactStrictMode: true,
+	webpack: (
+		config,
+		{ buildId, dev, isServer, defaultLoaders, nextRuntime }
+	) => {
+		if (!isServer) {
+			config.resolve.fallback = {
+				...config.resolve.fallback,
+
+				// ref: https://stackoverflow.com/q/67478532/6908282
+				// Fixes npm packages that depend on `fs` module
+				fs: false,
+			};
+		}
+
+		return config;
+	},
 };
 
 export default nextConfig;
