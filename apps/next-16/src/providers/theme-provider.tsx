bc0041383f1ec2caab6428d33d0bfc34dfc9bd2d
Anwesh Gangula <gangulaanwesh@gmail.com>
2025-12-11 00:09:02 +0530

    fix hydration error from next-themes

--- apps/next-16/src/providers/theme-provider.tsx
+++ apps/next-16/src/providers/theme-provider.tsx
@@ -1,10 +1,16 @@
 'use client';
 // ref: https://ui.shadcn.com/docs/dark-mode/next
+// ref: https://github.com/shadcn-ui/ui/issues/6757#issuecomment-3354607187
 
-import {
-	ThemeProviderProps as NextThemeProviderProps,
-	ThemeProvider as NextThemesProvider,
-} from 'next-themes';
+import type { ThemeProviderProps as NextThemeProviderProps } from 'next-themes';
+import dynamic from 'next/dynamic';
+
+const NextThemesProvider = dynamic(
+	() => import('next-themes').then((e) => e.ThemeProvider),
+	{
+		ssr: false,
+	}
+);
 
 export function ThemeProvider({ children, ...props }: NextThemeProviderProps) {
 	return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
