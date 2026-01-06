0a70c372dc3448658938f47ef07d6b9c969307d1
Anwesh Gangula <gangulaanwesh@gmail.com>
2026-01-07 01:36:15 +0530

    setup basic structure to export library package

--- packages/adobe-leonardo-colors/vite.config.ts
+++ packages/adobe-leonardo-colors/vite.config.ts
@@ -1,7 +1,19 @@
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
-// https://vite.dev/config/
+// ref: https://dev.to/receter/how-to-create-a-react-component-library-using-vites-library-mode-4lma
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), dts({ include: ['lib'] })],
+	build: {
+		copyPublicDir: false,
+		rollupOptions: {
+			external: ['react', 'react/jsx-runtime'],
+		},
+		lib: {
+			entry: resolve(__dirname, 'lib/main.ts'),
+			formats: ['es'],
+		},
+	},
 });
