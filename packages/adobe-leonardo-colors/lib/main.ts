dafc4cd8b7213276577c441a909074c05fe3378a
Anwesh Gangula <gangulaanwesh@gmail.com>
2026-01-07 22:20:36 +0530

    change code to build with types

--- packages/adobe-leonardo-colors/lib/main.ts
+++ packages/adobe-leonardo-colors/lib/main.ts
@@ -1,3 +1,3 @@
-export { Button } from './components/button';
-export { Input } from './components/input';
-export { Label } from './components/label';
+export { Button } from '@/components/button';
+export { Input } from '@/components/input';
+export { Label } from '@/components/label';
--- packages/adobe-leonardo-colors/vite.config.ts
+++ packages/adobe-leonardo-colors/vite.config.ts
@@ -2,10 +2,29 @@ import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // ref: https://dev.to/receter/how-to-create-a-react-component-library-using-vites-library-mode-4lma
 export default defineConfig({
-	plugins: [react(), dts({ include: ['lib'] })],
+	plugins: [
+		react(),
+		libInjectCss(),
+		dts({
+			include: ['lib'],
+			tsconfigPath: './tsconfig.build.json',
+			outDir: 'dist', // Explicitly tell the plugin to put types here
+			insertTypesEntry: true, // Creates a "types" field in your package.json (if missing)
+			copyDtsFiles: true,
+			afterBuild: () => {
+				console.log('Types generation complete!');
+			},
+		}),
+	],
+	resolve: {
+		alias: {
+			'@': resolve(__dirname, 'lib'),
+		},
+	},
 	build: {
 		copyPublicDir: false,
 		rollupOptions: {
